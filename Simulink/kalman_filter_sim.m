ccf2d2d093a9dbf9afe5f5d57aab98d9cf0ee522
Asbjørn <kykutura@gmx.com>
2024-12-10 13:21:31 +0100

    file til Henrik

--- Simulink/kalman_filter_sim.m
+++ Simulink/kalman_filter_sim.m
@@ -1,93 +1,81 @@
-function [x_hat,P] = kalman_filter_sim_l(time, olde_time, P_m1, Xp_m1,u_m1,y)
-    td = time - olde_time; %time delta
-    
-    bc = Xp_m1(3);
-    b = -800;
-    m = 1500;
-    
-    A = [0, 1, 0 ;
-         0, (b+bc)/m, 0;
-         0, 0,  0];
-    
-    B = [0, 0, 0;
-         0, 1/m, 0;
-         0, 0, 0];
-    
-    C = [1, 0, 0;
-         0, 0, 0;
-         0, 0, 0];
-
-    Q_m1 = diag([1e-5, 1e-5, 1e-3])
-    %Q_m1 = diag([1e-5, 1e-5, 1e-7]);  % Process noise covariance
-    R = diag([1e-2, 1e-2, 1e-1]);                      % Measurement noise covariance
-    
-    F_m1 = eye(size(A)) + A * td;
-    G_m1 = B* td;
-    H = C;
-    
-    I = eye(3)
-    Pm = F_m1*P_m1*F_m1'+Q_m1
-    K  = Pm*H'*inv(H*Pm*H'+R)
-    Xm = F_m1*Xp_m1+G_m1*u_m1
-    Xp = Xm + K*(y-H*Xm)
-    Pp = (I - K*H)*Pm*(I - K*H)' + K*R*K'
-
-    x_hat = Xp
-    P = Pp
-end
-
-function [x_1] = dynamik(time, olde_time, x,u)
-    td = time - olde_time; %time delta
-    m = 1500; %b = -1000;
-    A = [0, 1, 0 ; 0, x(3)/m, 0 ;0, 0,    0];
-        B = [0, 0, 0; 0, 1/m, 0; 0, 0, 0];
-    x_1 = A*x*td+B*u*td+x;
-end
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This is a simplified model of a boat in one dimension with a single thruster. i 
+% The state space includes position and velocity. 
+% The goal is to estimate the drag coefficient of the boat in one dimension.
+% The estimated parameter is used in an extended Kalman filter, implemented 
+% as an inner function, to see if we can accurately estimate the system parameters.
+% Written by Asbjørn.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all; close all; clc;
-N = 1000;
-time = zeros(1, N);
-ts = 0.1 %time step size.
-time = ts:ts:N*ts;
 
-x = zeros(3, N);        % True system dynamics.
-x(:, 1) = [0; -10; -1000];    % Initial state
-x_hat = zeros(3, N);
-x_hat(:, 1) = [0; -10; 0];
-p_hat = eye(3) * 1e-3;  % Small positive definite matrix
+N = 1000; % Number of simulation steps
+time = zeros(1, N); 
+ts = 0.1; % Time step size
+time = ts:ts:N * ts;
 
-u = zeros(3, N);   % Zero input force
-for i = 500:N; u(2,i) = sin(i*i*0.00006)*100000; end
-u(2,100:200) = 5000;
-u(2,400:750) = -700;
+% State variables: position, velocity, and drag coefficient
+x = zeros(3, N);        
+x(:, 1) = [0; -1; -1200];    % Initial state [position, velocity, drag coefficient]
 
+% Estimated state and covariance matrix initialization
+x_est = zeros(3, N);         % Estimated state
+x_est(:, 1) = [0; -0.5; 0];  % Initial guess for estimated state
+P_est = eye(3) * 1e-3;       % Initial covariance matrix
+
+% Input (control signal)
+u = zeros(3, N);   
+for i = 500:N
+    u(2, i) = sin(i  * 0.006) * 1000; 
+end
+u(2, 100:200) = 500;
+u(2, 400:750) = -700;
+
+% Loop through all time steps, calculate true dynamics, add measurement noise,
+% and apply the extended Kalman filter.
 for i = 2:N
-    x(:,i)      = dynamik(time(i), time(i-1), x(:,i-1), u(:,i-1)); % True system dynamics.
-    noise_meserment = x(:,i-1) + [1,0,0]*random('Normal', 0, 1, [3, 1])*1;
-    %[x_hat(:,i), p_hat] = kalman_filter_sim_l(time(i), time(i-1), p_hat ,x_hat(:,i-1), u(:,i-1), noise_meserment); % Estes Medt state.
-    [x_hat(:,i), p_hat] = extended_kalman_filter_sim(time(i), time(i-1), p_hat ,x_hat(:,i-1), u(:,i-1), noise_meserment);
+    % Compute true system dynamics
+    x(:, i) = dynamics(time(i), time(i-1), x(:, i-1), u(:, i-1)); 
+    
+    % Add noise to measurement
+    noisy_measurement = x(:, i-1) + [1, 0, 0] * random('Normal', 0, 1, [3, 1]) * 1;
+    
+    % Apply extended Kalman filter
+    [x_est(:, i), P_est] = extended_kalman_filter_sim(time(i), time(i-1), P_est, x_est(:, i-1), u(:, i-1), noisy_measurement);
 end
 
-
+% Plot results
 figure;
 subplot(3,1,1);
-plot(time, x(1, :), 'b', time, x_hat(1, :), 'r');
+plot(time, x(1, :), 'b', time, x_est(1, :), 'r');
 xlabel('Time (s)');
 ylabel('Position (m)');
 legend('True Position', 'Estimated Position');
 title('Position Estimation');
 
 subplot(3,1,2);
-plot(time, x(2, :), 'b', time, x_hat(2, :), 'r');
+plot(time, x(2, :), 'b', time, x_est(2, :), 'r');
 xlabel('Time (s)');
-ylabel('velocity. (m/s)');
-legend('True velocity', 'Estimated velocity');
-title('velocity Estimation');
+ylabel('Velocity (m/s)');
+legend('True Velocity', 'Estimated Velocity');
+title('Velocity Estimation');
 
 subplot(3,1,3);
-plot(time, x(3, :), 'b', time, x_hat(3, :)*1000, 'r');
+plot(time, x(3, :), 'b', time, x_est(3, :) - 1000, 'r');
 xlabel('Time (s)');
 ylabel('Drag Coefficient (b_c)');
 legend('True b_c', 'Estimated b_c');
 title('Drag Coefficient Estimation');
+
+% Function for true system dynamics
+function [x_next] = dynamics(current_time, previous_time, x, u)
+    delta_t = current_time - previous_time; % Time step
+    m = 1500; % Mass of the boat
+    A = [0, 1, 0; 
+         0, x(3)/m, 0; 
+         0, 0, 0];
+    B = [0, 0, 0; 
+         0, 1/m, 0; 
+         0, 0, 0];
+    x_next = A * x * delta_t + B * u * delta_t + x;
+end
